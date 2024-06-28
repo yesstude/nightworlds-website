25f4cf64d3f60fb5c21d8e562ad38ab7924b8fe5
ruslan.gulid <ruslan.gulid@gmail.com>
2024-06-28 07:17:57 +0000

    setup redirect

--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -1,10 +1,11 @@
 import { redirect } from "next/navigation";
-import { isAuthorized } from "../../../server/api/auth";
+import { isAuthorized, isSetupFinished } from "../../../server/api/auth";
 import { ReactNode } from "react";
 import DashboardWrapper from "../../../components/dashboard/DashboardWrapper";
 
 export default async function DashboardLayout(props: { children: ReactNode }) {
   if (!(await isAuthorized())) return redirect("/auth/signin");
+  if (!(await isSetupFinished())) return redirect("/setup");
 
   return <DashboardWrapper>{props.children}</DashboardWrapper>;
 }
