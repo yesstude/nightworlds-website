d5a0937ddd7917c634ff20c9995aa972304e8bf1
ruslan.gulid <ruslan.gulid@gmail.com>
2025-02-10 23:17:01 +0000

    bugfix

--- src/app/[locale]/(authorized-only)/setup/finish/layout.tsx
+++ src/app/[locale]/(authorized-only)/setup/finish/layout.tsx
@@ -1,13 +1,13 @@
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
-import { getMeUnsafe } from "~/server/api/sessions";
+import { getCurrentSession } from "~/server/api/sessions";
 
 export default async function SetupLayout({
   children,
 }: {
   children: ReactNode;
 }) {
-  const user = await getMeUnsafe();
+  const { user } = await getCurrentSession();
   if (!user!.licenseType) return redirect("/setup/license");
   if (!user!.nickname) return redirect("/setup/nickname");
   if (!user!.passwordHash && user!.licenseType != "online")
