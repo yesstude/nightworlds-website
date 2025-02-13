e19d17a16a6759141d1fb5f89515df588ffa4e17
ruslan.gulid <ruslan.gulid@gmail.com>
2025-02-13 15:40:00 +0000

    medium banner update

--- src/app/[locale]/(landing)/landing-banner.tsx
+++ src/app/[locale]/(landing)/landing-banner.tsx
@@ -6,11 +6,19 @@ import { AspectRatio } from "~/components/ui/aspect-ratio";
 import { LinkButton } from "~/components/transition/link";
 import { Icon } from "~/components/ui/icon";
 
+import medium from "../(authorized-only)/dashboard/worlds/nwm37.svg";
+
 export function LandingBanner() {
   return (
     <div className="overflow-none relative mb-[64px] mt-2 flex w-full max-w-[1400px] flex-col-reverse rounded-[32px] bg-primary/5 shadow-md sm:rounded-none sm:bg-transparent sm:p-[8px] sm:shadow-transparent md:px-[48px]">
       <div className="sm:dark inset-0 z-10 flex flex-col justify-end gap-4 p-[20px] sm:absolute sm:p-[32px] md:px-[96px] md:py-[56px]">
         <div className="max-w-[520px] p-1 text-[14px] font-medium leading-relaxed tracking-wide text-foreground sm:text-[18px] lg:max-w-[720px] lg:text-[24px]">
+          <Image
+            src={medium}
+            alt="Letter M cuboid logo"
+            loading="eager"
+            className="mb-2 hidden w-[80px] rounded-[24px] bg-foreground p-2 brightness-[125%] md:block lg:mb-6 lg:w-[128px] lg:rounded-[32px] lg:p-3"
+          />
           <h1 className="text-[18px] font-bold leading-tight tracking-normal sm:text-[32px] lg:text-[48px]">
             NightWorld Medium 3.7
           </h1>
@@ -20,7 +28,7 @@ export function LandingBanner() {
           <p>16 февраля в 19:00 МСК</p>
         </div>
         <LinkButton
-          className="hover:bg-primary sm:h-[50px] sm:shadow-lg lg:h-[72px] lg:px-[28px] lg:text-[24px]"
+          className="transition-shadow hover:bg-primary/95 hover:shadow-xl sm:h-[50px] sm:shadow-lg lg:h-[72px] lg:px-[28px] lg:text-[24px] [&_.icon]:hover:translate-x-1 lg:[&_.icon]:hover:translate-x-2"
           href="/dashboard/worlds"
         >
           Предзаказ
@@ -28,7 +36,7 @@ export function LandingBanner() {
             icon="arrow_right_alt"
             weight={200}
             size={32}
-            className="-mr-2 md:translate-x-1"
+            className="icon -mr-2 transition-transform lg:translate-x-1"
           />
         </LinkButton>
       </div>
@@ -37,7 +45,7 @@ export function LandingBanner() {
           src={photo}
           alt="Minecraft city screenshot"
           loading="eager"
-          className="h-full w-full rounded-t-[32px] object-cover brightness-[25%] sm:rounded-[32px] sm:shadow-xl lg:rounded-[86px]"
+          className="h-full w-full rounded-t-[32px] object-cover sm:rounded-[32px] sm:shadow-xl sm:brightness-[25%] lg:rounded-[86px]"
         />
       </AspectRatio>
     </div>
