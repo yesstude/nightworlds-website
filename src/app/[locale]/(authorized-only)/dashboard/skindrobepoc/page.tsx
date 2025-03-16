51c92eb5cfc2ca08fec0efed1f5cdd798e0d40a3
ruslan.gulid <ruslan.gulid@gmail.com>
2025-03-16 23:34:01 +0000

    skindrobe auth page

---
+++ src/app/[locale]/(authorized-only)/dashboard/skindrobepoc/page.tsx
@@ -0,0 +1,23 @@
+import { cookies } from "next/headers";
+import { GameserverIP } from "../(home)/gameserver-ip";
+
+export default function SkindrobePoCPage() {
+  const cs = cookies();
+  const session = cs.get("session");
+
+  return (
+    <div className="flex w-full flex-col gap-6 lg:p-8">
+      <h1 className="mb-4 text-[32px] font-bold leading-tight tracking-normal text-foreground">
+        Skindrobe Alpha
+      </h1>
+      <p>
+        Система Skindrobe ещё находится в очень ранней стадии тестирования. Для
+        подключения скопируйте токен сессии в приложение, поддерживающее
+        Skindrobe. Не разглашайте и не показывайте этот токен
+      </p>
+      <div>
+        <GameserverIP ip={session?.value ?? "не работает :("} />
+      </div>
+    </div>
+  );
+}
