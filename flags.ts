94f1079ae6efcedce309bc7a773d7ed556252a72
ruslan.gulid <ruslan.gulid@gmail.com>
2025-05-16 08:21:14 +0000

    removed the test file

--- flags.ts
+++
@@ -1,9 +0,0 @@
-import { Banner, DyeColor, Pattern } from "mcbanners";
-
-(async () => {
-  const b = new Banner(DyeColor.WHITE)
-    .add(Pattern.STRIPE_BOTTOM, DyeColor.YELLOW)
-    .add(Pattern.STRIPE_TOP, DyeColor.LIGHT_BLUE);
-
-  console.log(b.toString());
-})();
