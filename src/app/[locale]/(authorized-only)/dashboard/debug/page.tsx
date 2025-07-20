aef610a8572b1e6e9b917e5e5f4e4776b3ff1970
Ruslan Gulid <71387747+yesstude@users.noreply.github.com>
2025-07-20 13:56:01 +0000

    Remove Servers component from DashboardDebugPage as the drizzle studio works again

--- src/app/[locale]/(authorized-only)/dashboard/debug/page.tsx
+++ src/app/[locale]/(authorized-only)/dashboard/debug/page.tsx
@@ -126,7 +126,6 @@ export default async function DashboardDebugPage() {
           </FastButton>
         </div>
       </div>
-      <Servers />
       <Players />
     </div>
   );
@@ -155,100 +154,6 @@ async function Players() {
           </div>
         ))}
       </div>
-    </div>
-  );
-}
-
-async function Servers() {
-  const entries = Object.entries(serversTable).filter((v) => !!v[1].columnType);
-  const servers = Object.entries(await db.select().from(serversTable));
-
-  return (
-    <div>
-      <h2 className="mb-3 text-[24px] font-bold leading-tight tracking-normal text-foreground">
-        Сервера миров
-      </h2>
-      <form
-        className="grid grid-flow-col place-items-center gap-4"
-        action={async (fd: FormData) => {
-          "use server";
-
-          const values = Object.fromEntries(fd.entries()) as {
-            [key in keyof typeof serversTable.$inferInsert]: string;
-          };
-
-          await db.insert(serversTable).values({
-            worldId: values.worldId as any,
-            isPreOrderable: Boolean(values.isPreOrderable),
-            overwriteWorldName:
-              values.overwriteWorldName!.length > 0
-                ? values.overwriteWorldName
-                : undefined,
-            startedAt: new Date(Number(values.startedAt)),
-          });
-          revalidateTag("debug");
-        }}
-      >
-        <select name="worldId" id="worldId">
-          <option value="proxy">proxy (not a world)</option>
-          {(await getAllWorldIds()).map((v) => (
-            <option value={v}>{v}</option>
-          ))}
-        </select>
-        <Input name="overwriteWorldName" placeholder="overwriteWorldName" />
-        <div>
-          <input type="checkbox" name="isPreOrderable" id="isPreOrderable" />
-          <label htmlFor="isPreOrderable">isPreOrderable</label>
-        </div>
-        <Input
-          name="startedAt"
-          placeholder="startedAt"
-          defaultValue={Date.now()}
-        />
-        <Button type="submit">Создать</Button>
-      </form>
-      <table className="[&_td]:px-6 [&_td]:py-2">
-        <thead>
-          <tr>
-            {entries.map((v) => (
-              <td>
-                {v[0]} ({v[1].columnType})
-              </td>
-            ))}
-            <td>delete</td>
-          </tr>
-        </thead>
-        <tbody>
-          {servers.map((s) => (
-            <tr>
-              {Object.entries(s[1]).map((v) => {
-                // try {
-                //   const c = JSON.stringify(v[1]);
-                //   return <td>{c}</td>;
-                // } catch (error) {
-                return <td>{v[1]?.toString()}</td>;
-                // }
-              })}
-              <td>
-                <form
-                  action={async () => {
-                    "use server";
-
-                    await db
-                      .delete(serversTable)
-                      .where(eq(serversTable.id, s[1].id));
-                    revalidateTag("debug");
-                  }}
-                >
-                  <Button variant="outlined" size="fab">
-                    <Icon icon="delete" />
-                  </Button>
-                </form>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
     </div>
   );
 }
